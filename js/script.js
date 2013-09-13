c32f91452836fdbddbc948ada4a11dfcfce14cd1
Leila <l.chancurrie@gmail.com>
2013-09-13 05:15:04 +0000

    Got back/forward buttons to work

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function showSection(sectionName) {
+function showSection(sectionName, newLoad) {
     var sectionList = ["home", "projects", "meetings", "events", "sponsors"]
     
     // Remove given section from list
@@ -18,4 +18,31 @@ function showSection(sectionName) {
         var currSection = document.getElementsByClassName(currSectionName);
         currSection[0].className = 'row-fluid content hidden ' + currSectionName;
     }
+
+    if (newLoad) {
+        // Change url and state
+        var stateObj = { foo: 'bar' };
+        history.pushState(stateObj, sectionName, '/' + sectionName);
+    }
+};
+
+function showInitialSection(newLoad) {
+    var url = document.location.href;
+    
+    if (url.indexOf('projects') != -1) {
+        showSection('projects', newLoad);
+    } else if (url.indexOf('meetings') != -1) {
+        showSection('meetings', newLoad);
+    } else if (url.indexOf('events') != -1) {
+        showSection('events', newLoad);
+    } else if (url.indexOf('sponsors') != -1) {
+        showSection('sponsors', newLoad);
+    } else {
+        showSection('home', newLoad);
+    }
+
+};
+
+window.onpopstate = function(event) {
+    showInitialSection(false);
 };
